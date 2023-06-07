7d15f0afe450c5cbd284af5ae000ff67ae34bcdb
Javier Gomez Davo <javierillo756@gmail.com>
2023-06-07 20:22:53 +0200

    Login y welcome screen

--- ProgramacionInterfaz/src/GUI/WelcomeScreen.java
+++ ProgramacionInterfaz/src/GUI/WelcomeScreen.java
@@ -1,21 +1,21 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
-public class Login extends JDialog {
+public class WelcomeScreen extends JDialog {
     private JPanel principal;
-    private JPanel panelCampos;
-    private JTextField inpUserName;
-    private JTextField inpPassword;
-    private JButton LOGINButton;
     private JButton buttonOK;
     private JButton buttonCancel;
+    private JPanel panelCampos;
+    private JButton adminButton;
+    private JButton userButton;
 
-    public Login() {
+    public WelcomeScreen() {
         setContentPane(principal);
-        setModal(true);
         getRootPane().setDefaultButton(buttonOK);
+        setModal(true);
 
         // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
@@ -31,6 +31,25 @@ public class Login extends JDialog {
                 onCancel();
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+
+        adminButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setModal(false);
+                JDialog dialog = new Login();
+                dialog.setTitle("Agregar Usuario");
+                dialog.setSize(600, 500);
+                dialog.setLocationRelativeTo(null);
+                dialog.setVisible(true);
+            }
+        });
+        userButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 
     private void onOK() {
@@ -44,7 +63,7 @@ public class Login extends JDialog {
     }
 
     public static void main(String[] args) {
-        Login dialog = new Login();
+        WelcomeScreen dialog = new WelcomeScreen();
         dialog.pack();
         dialog.setVisible(true);
         System.exit(0);
